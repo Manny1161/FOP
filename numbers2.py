ab589d66b19c3f8d44ae1ce1143556e606747960
Manny1161 <43110360+Manny1161@users.noreply.github.com>
2022-07-16 22:23:43 +0800

    Delete numbers2.py

--- numbers2.py
+++
@@ -1,9 +0,0 @@
-print('Enter ten numbers...')
-total = 0
-
-for i in range(10):
-    print('Enter a number (', i, ')...')
-    number = int(input())
-    total = total + number
-
-print('Total is', total)
